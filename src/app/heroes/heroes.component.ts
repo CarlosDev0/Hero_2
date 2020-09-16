a8419058728e32f35d123180a472aa3f26126e64
carlossan1978 <carlossan@gmail.com>
2020-09-15 21:50:44 -0500

    Paso 2: Display a List of Tutorial: Tour of Heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
+
+  heroes = HEROES;
+  selectedHero: Hero;
 
   constructor() { }
   hero: Hero ={
@@ -15,5 +20,8 @@ export class HeroesComponent implements OnInit {
   };
   ngOnInit(): void {
   }
-
+  
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
